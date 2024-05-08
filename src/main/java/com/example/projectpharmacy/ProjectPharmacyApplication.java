9a49407229f6c77eea996eb00c1c3d421950b871
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-05-08 12:51:52 +0900

    first commit

---
+++ src/main/java/com/example/projectpharmacy/ProjectPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectpharmacy/ProjectPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
