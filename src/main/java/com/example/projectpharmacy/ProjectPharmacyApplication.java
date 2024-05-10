9772073b5d0475fc373705ced04bac8d3d985de8
Khyojun <khyojun1104@gmail.com>
2024-05-10 15:24:44 +0900

    BaseTimeEntity 관련하여 추가

--- src/main/java/com/example/projectpharmacy/ProjectPharmacyApplication.java
+++ src/main/java/com/example/projectpharmacy/ProjectPharmacyApplication.java
@@ -2,8 +2,10 @@ package com.example.projectpharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProjectPharmacyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/projectpharmacy/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/projectpharmacy/pharmacy/entity/Pharmacy.java
@@ -1,5 +1,6 @@
 package com.example.projectpharmacy.pharmacy.entity;
 
+import com.example.projectpharmacy.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 이 안에 비우면 db 기본 전략에 따름
